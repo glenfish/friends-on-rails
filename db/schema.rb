28538b06a1f9291fe0d03d23775b9b28f14ef42b
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2021-01-06 21:24:05 +1100

    initial commit

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2021_01_06_052709) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.string "email"
+    t.string "twitter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
